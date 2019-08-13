f538bc68f6bdb4ced8ca0bc6413e4dadd5df5d3d
wongband <bryanwong7788@gmail.com>
2019-08-12 17:34:53 -0700

    initial

---
+++ templates/static/js/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className='header-page'>
+                <img className='ui logo image' src={this.props.logo}/>
+                <span className='header'>
+                   Medium Articles
+                </span>
+            </div>
+        );
+    }
+}
